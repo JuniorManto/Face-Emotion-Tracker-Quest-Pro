5c66356429073ecfde0f817bd34513c8ac3d35a1
Shehata <ahmedsshehata03@gmail.com>
2026-07-06 19:25:22 -0700

    add: firebase integration

---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Globalization;
+using Firebase.Database;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour
+{
+    private DatabaseReference _reference;
+
+    private void Start()
+    {
+        var options = new Firebase.AppOptions();
+        options.ApiKey = "AIzaSyDOXeUKSzyUnC7gu6ONKd28joPdrmU9Ud8";
+        options.AppId = "1:94282840552:android:c02a7523c43a3a064f0c5d";
+        options.DatabaseUrl = new Uri("https://face-emotion-tracker-default-rtdb.firebaseio.com");
+        options.ProjectId = "face-emotion-tracker";
+        options.StorageBucket = "face-emotion-tracker.firebasestorage.app";
+
+        var app = Firebase.FirebaseApp.Create(options);
+        _reference = FirebaseDatabase.GetInstance(app).RootReference;
+
+        StartCoroutine(ReadHappinessThreshold());
+    }
+
+    public IEnumerator ReadHappinessThreshold()
+    {
+        var dbTask = _reference.Child("happiness").GetValueAsync();
+        yield return new WaitUntil(() => dbTask.IsCompleted);
+
+        if (dbTask.Exception != null)
+        {
+            Debug.LogError(dbTask.Exception);
+            yield break;
+        }
+
+        DataSnapshot snapshot = dbTask.Result;
+        
+        Debug.Log("Happiness threshold is: " + snapshot.Value);
+    }
+    
+    private string GetTime()
+    {
+        return DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss", CultureInfo.InvariantCulture);
+    }
+}
